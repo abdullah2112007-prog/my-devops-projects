1ce4721bc185031129f908eaf375c8ff5490b972
Abdullah <abdullah2112007@gmail.com>
2026-09-17 21:53:09 +0530

    Add Docker network message

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import Flask
-
-app = Flask(__name__)
-
 @app.route("/")
 def home():
     return """
     <h1>🚀 Welcome Abdullah!</h1>
     <h2>Cloud & DevOps Learning</h2>
     <p>Days Completed: 45</p>
+    <p>🌐 Docker Network Practice Completed!</p>
     """
-
-app.run(host="0.0.0.0", port=5000)
